97fccc46cc30b3dd2734101d4eb3730ad00d6357
Gabriel221011 <gcalvoa01@educantabria.es>
2026-01-15 10:09:45 +0100

    Delete Terraform-ejemplos directory

--- Terraform-ejemplos/05-CREAR-NODOS-ANSIBLE/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ansible_nodes_public_ips" {
-  value = aws_instance.ansible_node[*].public_ip
-  description = "IPs públicas de las instancias para Ansible"
-}
--- Terraform-ejemplos/05-CREAR-NODOS-ANSIBLE/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-aws_region       = "us-east-1"
-aws_profile      = "laboratorio-151777"
-instance_type    = "t3.micro"
-
-
